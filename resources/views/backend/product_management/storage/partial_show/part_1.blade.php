72f6d4cb02ef33997dfad3e6424942bf43a9518b
LabibArefin123 <ah947777@gmail.com>
2026-03-05 14:37:06 +0600

    show page separated

--- resources/views/backend/product_management/storage/partial_show/part_1.blade.php
+++ resources/views/backend/product_management/storage/partial_show/part_1.blade.php
@@ -1,45 +1,37 @@
  {{-- Start of Product Part --}}
  <div class="row ">
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>Product Name:</strong>
          <p class="form-control">
              {{ $storage->product->name }}
          </p>
      </div>
 
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>SKU:</strong>
          <p class="form-control">
              {{ $storage->product->sku }}
          </p>
      </div>
 
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>Part Number</strong>
-         <p class="form-control">
-             {{ $storage->product->part_number }}
-         </p>
+          <input type="text" id="name" class="form-control" value="{{ $storage->product->part_number }}" readonly>
      </div>
 
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>Type / Model</strong>
-         <p class="form-control">
-             {{ $storage->product->type_model }}
-         </p>
+         <input type="text" id="name" class="form-control" value="{{ $storage->product->type_model }}" readonly>
      </div>
 
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>Origin</strong>
-         <p class="form-control">
-             {{ $storage->product->origin }}
-         </p>
+         <input type="text" id="name" class="form-control" value="{{ $storage->product->origin }}" readonly>
      </div>
 
-     <div class="col-md-4">
+     <div class="col-md-4 form-group">
          <strong>Using Place</strong>
-         <p class="form-control">
-             {{ $storage->product->using_place }}
-         </p>
+         <input type="text" id="name" class="form-control" value="{{ $storage->product->using_place }}" readonly>
      </div>
  </div>
  {{-- End of Product Part --}}
--- resources/views/backend/product_management/storage/partial_show/part_2.blade.php
+++ resources/views/backend/product_management/storage/partial_show/part_2.blade.php
@@ -33,77 +33,56 @@
      <input type="text" id="name" class="form-control" value=" {{ $storage->alert_quantity }}" readonly>
  </div>
 
-     <div class="col-md-12">
+ <div class="col-md-12">
+     <table class="table table-bordered table-sm text-center">
+         <thead class="table-light">
+             <tr>
+                 <th style="width:50%">Product Image</th>
+                 <th style="width:50%">Product Barcode</th>
+             </tr>
+         </thead>
 
-         <table class="table table-bordered table-sm text-center">
-             <thead class="table-light">
-                 <tr>
-                     <th style="width:50%">Product Image</th>
-                     <th style="width:50%">Product Barcode</th>
-                 </tr>
-             </thead>
+         <tbody>
+             <tr>
+                 <td>
+                     @if ($storage->image_path)
+                         <img src="{{ asset($storage->image_path) }}" class="img-fluid img-thumbnail"
+                             style="max-height:120px;">
+                     @else
+                         <span class="text-muted small">No Image</span>
+                     @endif
+                 </td>
 
-             <tbody>
-                 <tr>
+                 <!-- BARCODE -->
+                 <td>
+                     @if ($storage->barcode_path)
+                         <img src="{{ asset($storage->barcode_path) }}" class="img-fluid" style="max-height:80px;">
 
-                     <!-- PRODUCT IMAGE -->
-                     <td>
+                         <div class="mt-1">
+                             <small class="text-muted">
+                                 {{ $storage->barcode }}
+                             </small>
+                         </div>
+                     @else
+                         <span class="text-muted small">No Barcode</span>
+                     @endif
+                 </td>
+             </tr>
+         </tbody>
+     </table>
+ </div>
 
-                         @if ($storage->image_path)
-                             <img src="{{ asset($storage->image_path) }}" class="img-fluid img-thumbnail"
-                                 style="max-height:120px;">
-                         @else
-                             <span class="text-muted small">No Image</span>
-                         @endif
+ <div class="col-md-4 form-group">
+     <strong>Stock Quantity:</strong>
+     <input type="text" id="name" class="form-control" value=" {{ $storage->stock_quantity }}" readonly>
+ </div>
 
-                     </td>
+ <div class="col-md-4 form-group">
+     <strong>Minimum Stock Level:</strong>
+     <input type="text" id="name" class="form-control" value=" {{ $storage->minimum_stock_level }}" readonly>
+ </div>
 
-
-                     <!-- BARCODE -->
-                     <td>
-
-                         @if ($storage->barcode_path)
-                             <img src="{{ asset($storage->barcode_path) }}" class="img-fluid" style="max-height:80px;">
-
-                             <div class="mt-1">
-                                 <small class="text-muted">
-                                     {{ $storage->barcode }}
-                                 </small>
-                             </div>
-                         @else
-                             <span class="text-muted small">No Barcode</span>
-                         @endif
-
-                     </td>
-
-                 </tr>
-             </tbody>
-         </table>
-
-     </div>
-
-     <!-- STOCK QUANTITY -->
-     <div class="col-md-4 form-group">
-         <strong>Stock Quantity:</strong>
-         <p class="form-control">
-             {{ $storage->stock_quantity }}
-         </p>
-     </div>
-
-
-     <!-- MIN STOCK -->
-     <div class="col-md-4 form-group">
-         <strong>Minimum Stock Level:</strong>
-         <p class="form-control">
-             {{ $storage->minimum_stock_level }}
-         </p>
-     </div>
-
-
-     <!-- MAX STOCK -->
-     <div class="col-md-4 form-group">
-         <strong>Maximum Stock Level:</strong>
-         <p class="form-control">
-             {{ $storage->maximum_stock_level }}
-         </p>
-     </div>
+ <div class="col-md-4 form-group">
+     <strong>Maximum Stock Level:</strong>
+     <input type="text" id="name" class="form-control" value=" {{ $storage->maximum_stock_level }}" readonly>
+ </div>
